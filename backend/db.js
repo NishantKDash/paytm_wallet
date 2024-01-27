bb49b5f3e3c901f25d1dff5e4bbf5b375f15b087
Nishant <nishantkumardash26@gmail.com>
2024-01-27 19:22:28 +0530

    config

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-mongoose.connect(dbUrl);
+const dbURL = require("./config");
+mongoose.connect(dbURL);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
