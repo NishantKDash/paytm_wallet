27ddf2a3c48e4833c04bdaa046f59fe596b78b3b
Nishant <nishantkumardash26@gmail.com>
2024-01-29 18:50:54 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Send from './components/Send'
+import Signin from './components/Signin'
+import Signup from './components/Signup'
 function App() {
 
   return (
     <div>
-        Hello world
+       <BrowserRouter>
+         <Routes>
+            <Route path = '/' element={<Dashboard></Dashboard>}></Route>
+            <Route path = '/signup' element={<Signup></Signup>}></Route>
+            <Route path = '/signin' element={<Signin></Signin>}></Route>
+            <Route path = '/send' element = {<Send></Send>}></Route>
+         </Routes>
+       </BrowserRouter>
     </div>
   )
 }
