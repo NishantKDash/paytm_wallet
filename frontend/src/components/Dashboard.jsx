b836c9577c06f71477ec2ea959b904bbf2dc0aef
Nishant <nishantkumardash26@gmail.com>
2024-01-30 00:16:53 +0530

    completed components

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,37 @@
-export default function Dashboard()
-{
-    return(
-        <div>
-            Dashboard
-        </div>
-    )
+export default function Dashboard() {
+  let users = ["Nishant Dash", "H Roshan", "Harkirat Singh", "D Bachchan"];
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-10 lg:px-8">
+      <div className="bg-slate-300 border border-sky-500 rounded-lg mx-1 flex items-center justify-between p-4 shadow-lg">
+        <h1 className="text-lg font-bold">Paytm</h1>
+        <h2 className="text-lg font-bold">Hi User</h2>
+      </div>
+      <div className="bg-black rounded  w-56 my-4 mx-1 shadow-lg">
+        <h2 className="font-bold text-white mx-1">Your Balance : INR 3000</h2>
+      </div>
+      <div className="my-2 mx-1 shadow-lg">
+        <input
+          className="block w-full rounded-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="Find users"
+        ></input>
+      </div>
+      {users.map((user) => {
+        return (
+          <div className="grid grid-cols-10 gap-4 my-10">
+            <div className="flex col-span-9 p-4">
+              <div className="bg-slate-200 rounded-full px-6 py-4">
+                {user[0]}
+              </div>
+              <p className="font-bold p-4">{user}</p>
+            </div>
+            <div className="col-span-1 p-4">
+              <button className="bg-black text-white rounded-lg p-4">
+                Send Money
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- frontend/src/components/Send.jsx
+++ frontend/src/components/Send.jsx
@@ -1,8 +1,23 @@
-export default function Send()
-{
-    return(
-        <div>
-            Send
-        </div>
-    )
+export default function Send() {
+  return (
+    <div className="h-1/3 w-1/3 justify-center text-center px-6 py-10 lg:px-8 bg-slate-100 shadow-lg">
+      <h1 className="text-2xl font-bold">Send Money</h1>
+      <div className="flex py-6">
+        <div className="bg-orange-500 rounded-full px-6 py-4">H</div>
+        <p className="font-bold p-4">Harkirat Singh</p>
+      </div>
+      <div>
+        <label className="block font-bold text-left">Amount (in Rs)</label>
+        <input
+          className="block w-full my-4 h-10 rounded-md border-2"
+          placeholder="Enter Amount"
+          type="number"
+          min="0"
+        ></input>
+        <button className="bg-green-500 w-full h-10 rounded-md text-white hover:bg-green-700 ">
+          Initate Transfer
+        </button>
+      </div>
+    </div>
+  );
 }
